5451ab1ac0690f60b11a1c8f4e098063f31f8228
Vic Amuso <vic@parablesoft.com>
2020-05-07 12:20:37 -0400

    Added attachments to mailer

--- app/mailers/stardust/hooks/hooks_mailer.rb
+++ app/mailers/stardust/hooks/hooks_mailer.rb
@@ -1,6 +1,9 @@
 class Stardust::Hooks::HooksMailer < ApplicationMailer
   add_template_helper(ActionView::Helpers::NumberHelper)
-  def generic_message(to:,subject:,content:, cc:nil, bcc:nil)
+  def generic_message(to:,subject:,content:, cc:nil, bcc:nil,attachments:[])
+    attachments.each do |k,v|
+      self.attachments[k] = v
+    end
     @content = content
     mail(to: to, cc: cc, subject: subject)
   end
